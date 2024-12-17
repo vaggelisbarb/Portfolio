b8d47bdd3dac8a359c2d5418fc923af70e74b1aa
Vaggelis Brb <vaggelisbarbalias@outlook.com>
2024-12-17 17:28:59 +0200

    Frontend changes. Features added

---
+++ javascript/page/portfolio.js
@@ -0,0 +1,36 @@
+downloadCV();
+redirectToGihub();
+redirectToLinkedin();
+
+function downloadCV() {
+    // Implement download CV functionality here
+    
+    document.querySelector('.portfolio__button--download-cv')
+        .addEventListener('click', () => {
+            console.log('Download CV button clicked');
+            const link = document.createElement('a');
+
+            link.href = '../public/resume/Barbalias Evangelos_Resume_2024.pdf';
+            link.download = 'Barbalias_Evangelos_Resume_2024.pdf';
+
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        });
+}
+
+function redirectToGihub() {
+    document.querySelector('.portfolio__social-link--github')
+        .addEventListener('click', () => {
+            console.log('GitHub img clicked');
+            window.location.href = 'https://github.com/vaggelisbarb';
+        });
+}
+
+function redirectToLinkedin() {
+    document.querySelector('.portfolio__social-link--linkedin')
+        .addEventListener('click', () => {
+            console.log('LinkedIn img clicked');
+            window.location.href = 'https://www.linkedin.com/in/evangelos-barmpalias-488b3b18b/';
+        });
+}
